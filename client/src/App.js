0fb4aad2338d7bf389f2b8542603717e455d53f3
Jongwon Park <jwp7073@gmail.com>
2022-10-25 14:40:53 +0900

    기능: 라우터 초기 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+
 function App() {
   return (
-    <div></div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
