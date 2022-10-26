0f37c6f059ae56e57863daa2c04b21f764917d6e
Mintaek_CHO <mintaek5555@gmail.com>
2022-10-26 21:50:45 +0900

    기능 : Answer Dto 클래스 구현
    AnswerPostDto, AnswerPatchDto, AnswerResponseDto 클래스를 구현하였음.
    
    #9

--- server/src/main/java/com/codestates/preproject/domain/answer/dto/AnswerPostDto.java
+++ server/src/main/java/com/codestates/preproject/domain/answer/dto/AnswerPostDto.java
@@ -1,7 +1,11 @@
 package com.codestates.preproject.domain.answer.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class AnswerPostDto {
+
+    private String body;
 }
--- server/src/main/java/com/codestates/preproject/domain/answer/dto/AnswerResponseDto.java
+++ server/src/main/java/com/codestates/preproject/domain/answer/dto/AnswerResponseDto.java
@@ -1,9 +1,13 @@
 package com.codestates.preproject.domain.answer.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class AnswerResponseDto {
+    private long answerId;
+    private String body;
 }
