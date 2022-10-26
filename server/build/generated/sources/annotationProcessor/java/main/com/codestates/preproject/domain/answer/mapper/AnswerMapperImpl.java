cfa9b895ea7f6c1291225334e30d1ab1fb8b53ed
park-yeong-ki <okip0428@gmail.com>
2022-10-27 02:46:31 +0900

    기능: application-aws.yml 파일 추가
    #15

--- server/build/generated/sources/annotationProcessor/java/main/com/codestates/preproject/domain/answer/mapper/AnswerMapperImpl.java
+++ server/build/generated/sources/annotationProcessor/java/main/com/codestates/preproject/domain/answer/mapper/AnswerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-27T02:16:14+0900",
+    date = "2022-10-27T02:44:30+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 11.0.16.1 (Azul Systems, Inc.)"
 )
 @Component
