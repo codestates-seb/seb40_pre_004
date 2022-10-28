4e22b870da0b4d76845670a0f0e4493cb58b3f11
Mintaek_CHO <mintaek5555@gmail.com>
2022-10-28 16:48:26 +0900

    기능 : JWT 로그인 인증 구현
    JWT 로그인 인증을 위해 Member- Service, Entity 코드가 일부 변경되었고,  LogonDto 클래스 및 그 외securet 관련 클래스가 추가됨
    
    #36

--- server/build/generated/sources/annotationProcessor/java/main/com/codestates/preproject/domain/question/mapper/QuestionMapperImpl.java
+++ server/build/generated/sources/annotationProcessor/java/main/com/codestates/preproject/domain/question/mapper/QuestionMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-27T17:27:04+0900",
+    date = "2022-10-28T16:40:12+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 11.0.16.1 (Azul Systems, Inc.)"
 )
 @Component
