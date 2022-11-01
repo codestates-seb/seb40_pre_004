e5e29ab73119ae087291974ddb47de1fc9e6c614
hyeleeeeen <heo22969087@gmail.com>
2022-11-01 22:52:04 +0900

    기능 : 상세페이지 연결

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import NewQuestion from './pages/NewQuestion';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Detail from './pages/Detail';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/detail" element={<Detail />} />
         <Route path="/questions/ask" element={<NewQuestion />} />
       </Routes>
     </>
