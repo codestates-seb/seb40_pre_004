9d81d8a6d247826901e2e73ef081f791530f92fe
Jongwon Park <jwp7073@gmail.com>
2022-11-01 16:12:36 +0900

    기능: 계정 복구 페이지, 회원가입 성공 페이지 route 연결

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import NewQuestion from './pages/NewQuestion';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import AccountRecovery from './pages/AccountRecovery';
+import RegisterSuccess from './pages/RegisterSuccess';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/account-recovery" element={<AccountRecovery />} />
+        <Route path="/register-success" element={<RegisterSuccess />} />
         <Route path="/questions/ask" element={<NewQuestion />} />
       </Routes>
     </>
