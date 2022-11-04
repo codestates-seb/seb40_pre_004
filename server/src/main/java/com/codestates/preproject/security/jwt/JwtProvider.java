57c0cd118413163f6d4dd6b400152b92eed5290d
park-yeong-ki <okip0428@gmail.com>
2022-11-05 00:19:58 +0900

    리팩토링: 토큰 발급 메서드 수정 및 리팩토링

--- server/src/main/java/com/codestates/preproject/security/jwt/JwtProvider.java
+++ server/src/main/java/com/codestates/preproject/security/jwt/JwtProvider.java
@@ -9,16 +9,19 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpClientErrorException.Forbidden;
 
 import javax.annotation.PostConstruct;
-import javax.naming.AuthenticationException;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.time.Duration;
 import java.util.Base64;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Objects;
 
@@ -30,17 +33,31 @@ public class JwtProvider {
     private final ObjectMapper objectMapper;
 
     @Value("${jwt.secret-key}")
-    private String key;
+    private String secretKey;
 
     @Value("${jwt.access-token-expiration-minutes}")
-    private Long atkLive;
+    private int accessTokenExpirationMinutes;
 
     @Value("${jwt.refresh-token-expiration-minutes}")
-    private Long rtkLive;
+    private int refreshTokenExpirationMinutes;
 
-    @PostConstruct
-    protected void init() {
-        key = Base64.getEncoder().encodeToString(key.getBytes());
+    public String encodeBase64SecretKey(String secretKey) {
+        return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public Date getTokenExpiration(int expirationMinutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, expirationMinutes);
+        Date expiration = calendar.getTime();
+
+        return expiration;
+    }
+
+    private Key getKeyFromBase64EncodedKey(String base64EncodedSecretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(base64EncodedSecretKey);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+
+        return key;
     }
 
     public TokenResponseDto createTokensByLogin(MemberResponseDto memberResponseDto) throws JsonProcessingException {
@@ -52,38 +69,45 @@ public class JwtProvider {
                 memberResponseDto.getMemberId(),
                 memberResponseDto.getEmail(),
                 memberResponseDto.getDisplayName());
-        String atk = createToken(atkSubject, atkLive);
-        String rtk = createToken(rtkSubject, rtkLive);
-        redisDao.setValues(memberResponseDto.getEmail(), rtk, Duration.ofMillis(rtkLive));
+        String atk = createToken(atkSubject, accessTokenExpirationMinutes);
+        String rtk = createToken(rtkSubject, refreshTokenExpirationMinutes);
+        redisDao.setValues(memberResponseDto.getEmail(), rtk, Duration.ofMinutes((long) refreshTokenExpirationMinutes));
         return new TokenResponseDto(atk, rtk);
     }
 
-    private String createToken(Subject subject, Long tokenLive) throws JsonProcessingException {
+    private String createToken(Subject subject, int tokenLive) throws JsonProcessingException {
         String subjectStr = objectMapper.writeValueAsString(subject);
         Claims claims = Jwts.claims()
                 .setSubject(subjectStr);
-        Date date = new Date();
+
+        Key key = getKeyFromBase64EncodedKey(encodeBase64SecretKey(secretKey));
+
         return Jwts.builder()
                 .setClaims(claims)
-                .setIssuedAt(date)
-                .setExpiration(new Date(date.getTime() + tokenLive))
-                .signWith(SignatureAlgorithm.HS256, key)
+                .setIssuedAt(Calendar.getInstance().getTime())
+                .setExpiration(getTokenExpiration(tokenLive))
+                .signWith(key)
                 .compact();
     }
 
-    public Subject getSubject(String atk) throws JsonProcessingException {
-        String subjectStr = Jwts.parser().setSigningKey(key).parseClaimsJws(atk).getBody().getSubject();
+    public Subject getSubject(String token) throws JsonProcessingException {
+        Key key = getKeyFromBase64EncodedKey(encodeBase64SecretKey(secretKey));
+
+        String subjectStr = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
         return objectMapper.readValue(subjectStr, Subject.class);
     }
 
     public TokenResponseDto reissueAtk(MemberResponseDto memberResponseDto) throws JsonProcessingException {
         String rtkInRedis = redisDao.getValues(memberResponseDto.getEmail());
-        if (Objects.isNull(rtkInRedis)) throw new JwtException("인증 정보가 만료되었습니다.");
+        if (Objects.isNull(rtkInRedis)) {
+            throw new JwtException("인증 정보가 만료되었습니다.");
+        }
+
         Subject atkSubject = Subject.atk(
                 memberResponseDto.getMemberId(),
                 memberResponseDto.getEmail(),
                 memberResponseDto.getDisplayName());
-        String atk = createToken(atkSubject, atkLive);
+        String atk = createToken(atkSubject, accessTokenExpirationMinutes);
         return new TokenResponseDto(atk, null);
     }
 }
